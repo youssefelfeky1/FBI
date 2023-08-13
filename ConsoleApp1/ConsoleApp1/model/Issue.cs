5c05c18a19f2500042877fece649256929107a9e
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-08-13 10:13:19 +0300

    PROJECT

---
+++ ConsoleApp1/ConsoleApp1/model/Issue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.model
+{
+    class Issue
+    {
+        private readonly string name;
+        private readonly string description;
+        private readonly string url;
+
+        Issue(string name, string description, string url)
+        {
+            this.name = name;
+            this.description = description;
+            this.url = url;
+        }
+    }
+}
