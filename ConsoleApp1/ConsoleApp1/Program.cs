007e908410fe198587a2d2892d44af3ddc09bcb8
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-08-13 12:01:58 +0300

    project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,33 @@
-﻿class Program
+﻿using ConsoleApp1.model;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Enter A to add new issue or S to search\n");
+        string input = Console.ReadLine();
+
+
+        if (input == "A" || input=="a")
+        {
+           string name;
+         string description;
+         string url;
+            Console.WriteLine("Enter Issue name\n");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Issue description\n");
+            description = Console.ReadLine();
+            Console.WriteLine("Enter Issue URL\n");
+            url = Console.ReadLine();
+            Issue issue = new(name, description, url);
+
+
+        }
+        else if(input=="s")
+        {
+            Console.WriteLine("Enter Issue name\n");
+            string name = Console.ReadLine();
+        }
+
     }
 }
--- ConsoleApp1/ConsoleApp1/model/Issue.cs
+++ ConsoleApp1/ConsoleApp1/model/Issue.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.model
 {
-    class Issue
+    public class Issue
     {
         private readonly string name;
         private readonly string description;
         private readonly string url;
 
-        Issue(string name, string description, string url)
+        public Issue(string name, string description, string url)
         {
             this.name = name;
             this.description = description;
