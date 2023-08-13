dcdc9e25da30015fc1673ea73dcd724e280a318d
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-08-13 13:04:39 +0300

    project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,29 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        Again:
         Console.WriteLine("Enter A to add new issue or S to search\n");
         string input = Console.ReadLine();
 
 
         if (input == "A" || input=="a")
         {
-           string name;
-         string description;
-         string url;
-            Console.WriteLine("Enter Issue name\n");
+            string name;
+            string description;
+            string url;
+            Console.WriteLine("Enter Issue name:");
             name = Console.ReadLine();
-            Console.WriteLine("Enter Issue description\n");
+            Console.WriteLine("Enter Issue description:");
             description = Console.ReadLine();
-            Console.WriteLine("Enter Issue URL\n");
+            Console.WriteLine("Enter Issue URL :");
             url = Console.ReadLine();
             Issue issue = new(name, description, url);
 
 
         }
-        else if(input=="s")
+        else if(input=="s"||input=="S")
         {
-            Console.WriteLine("Enter Issue name\n");
+            Console.WriteLine("Enter Issue name :");
             string name = Console.ReadLine();
+        }
+        else
+        {
+            goto Again;
         }
 
     }
