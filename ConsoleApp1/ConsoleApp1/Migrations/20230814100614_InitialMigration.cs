1adcd026072d2ef5df8d0d805f1129e28909be2e
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-08-14 14:36:26 +0300

    update

---
+++ ConsoleApp1/ConsoleApp1/Migrations/20230814100614_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Issues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issues", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Issues");
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,31 +1,84 @@
-﻿using ConsoleApp1.model;
+﻿using ConsoleApp1;
+using ConsoleApp1.model;
+using Microsoft.Data.SqlClient;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Again:
+       
+
+    Again:
         Console.WriteLine("Enter A to add new issue or S to search\n");
         string input = Console.ReadLine();
+        MyappContext myappContext=new MyappContext();
+        
 
 
         if (input == "A" || input=="a")
         {
-            string name;
-            string description;
-            string url;
+           
+
+            Issue issue = new Issue();
+
+
             Console.WriteLine("Enter Issue name:\n");
-            name = Console.ReadLine();
+            issue.name = Console.ReadLine();
+
             Console.WriteLine("Enter Issue description:\n");
-            description = Console.ReadLine();
+            issue.description = Console.ReadLine();
+
             Console.WriteLine("Enter Issue URL:\n");
-            url = Console.ReadLine();
-            Issue issue = new(name, description, url);
+            issue.url = Console.ReadLine();
+
+           
+
+            
+
+
+
+            myappContext.Issues.Add(issue);
+            myappContext.SaveChanges();
+
+           
+
+
+
+           
+
         }
         else if(input=="s"||input=="S")
         {
+
+            searchagain:
             Console.WriteLine("Enter Issue name :\n");
             string name = Console.ReadLine();
+
+
+
+
+
+            var retIssue = myappContext.Issues.Where(x => x.name == name);
+            
+            if(retIssue.Count()!=0)
+            {
+                Console.WriteLine("\nIssue name: " + retIssue.First().name);
+
+                Console.WriteLine("Issue description: " + retIssue.First().description);
+
+                Console.WriteLine("Issue URL: " + retIssue.First().url);
+            }
+            else
+            {
+                Console.WriteLine("Enter Correct name ,pls\n");
+                goto searchagain;
+            }
+
+
+
+           
+            
+
         }
         else
         {
